46c9525aa6a253f47d44a9c61bf176f439d91699
Carlos William <carloswtmachado@gmail.com>
2022-06-15 23:52:46 -0300

    adciona o get '/tweets', retorna os 10 ultimos tweets

--- index.js
+++ index.js
@@ -15,7 +15,64 @@ let tweets = [
 	{
 		username: 'bobesponja', 
 		tweet: "eu amo o hub"
+	},
+	{
+		username: 'bobesponja', 
+		tweet: "eu amo o hub"
+	},
+	{
+		username: 'bobesponja', 
+		tweet: "eu amo o hub"
+	},
+	{
+		username: 'bobesponja', 
+		tweet: "eu amo o hub"
+	},
+	{
+		username: 'bobesponja', 
+		tweet: "eu amo o hub"
+	},
+	{
+		username: 'bobesponja', 
+		tweet: "eu amo o hub"
+	},
+	{
+		username: 'bobesponja', 
+		tweet: "eu amo o hub"
+	},
+	{
+		username: 'bobesponja', 
+		tweet: "eu amo o hub"
+	},
+	{
+		username: 'bobesponja', 
+		tweet: "eu amo o hub"
+	},
+	{
+		username: 'bobesponja', 
+		tweet: "eu amo o hub"
+	},
+	{
+		username: 'bobesponja', 
+		tweet: "eu amo o hub"
 	}
 ];
+
+app.get("/tweets", (_, res) => {
+	let last_tweets = [];
+	for(let i = 0; i < tweets.length && i < 10; i++){
+		let avatar = "";
+		for(let j = 0; j < usuarios.length; j++){
+			if(tweets[i].username === usuarios[j].username)
+				avatar = usuarios[j].avatar;
+		}
+		last_tweets.push({
+			username: tweets[i].username,
+			avatar: avatar,
+			tweet: tweets[i].tweet
+		});
+	}
+	res.send(last_tweets);
+});
 
 app.listen(5000);
